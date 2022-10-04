1083bf5e5d105673fb977d81a362871cb828cf9f
Joels2812 <joelledez2812@gmail.com>
2022-10-03 23:30:08 -0600

    Code's comments

--- nyt.py
+++ nyt.py
@@ -1,16 +1,23 @@
+#imports
 import requests
 import pandas as pd
 
+#in this string i save the url of the API and then make the request of the url
 url = 'https://api.nytimes.com/svc/books/v3/lists/best-sellers/history.json?api-key=LrbCeftsODrEyj8rBZsT37Jd5mJk8Lt0'
 r = requests.get(url)
 
+#in this if i look if the request of the url was successful
 if r.status_code == 200:
     json= r.json()
     json.keys()
     jsonResults = json['results']
+    #pass the information to a dataframe 
     df = pd.DataFrame(jsonResults)
+    #filter the data
     dfClean = df[['title','description','author','publisher']]
+    #sort the data alphabetically
     dfClean =dfClean.sort_values('title',ascending=True)
+    #create the csv with the data
     dfClean.to_csv('nyt.csv')
     print("El csv se ha creado")
 else:
